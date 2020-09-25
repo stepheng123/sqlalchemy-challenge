cdf1f0b18ac3a0597547797179fafab32916c59c
Stephen Guilmartin <65271593+stepheng123@users.noreply.github.com>
2020-09-25 16:42:33 +0800

    Update app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy import distinct
+from flask import Flask, jsonify
+from sqlalchemy import and_
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+
+# 2. Create an app
+app = Flask(__name__)
